245ead6243421985488530428830c4f9ae554cc3
Vishnu Sanal. T <t.v.s10123@gmail.com>
2021-02-09 12:57:46 +0530

    MaxConsecutiveOnes

---
+++ src/MaxConsecutiveOnes.java
@@ -0,0 +1,23 @@
+public class MaxConsecutiveOnes {
+    public static void main(String[] args) {
+
+        int[] ints = new int[]{1, 1, 0, 1, 1, 1};
+
+        System.out.println(findMaxConsecutiveOnes(ints));
+    }
+
+    static int findMaxConsecutiveOnes(int[] ints) {
+        int current = 0, max = 0;
+
+        for (int i : ints) {
+            if (i == 1) {
+                current++;
+            } else {
+                max = Math.max(current, max);
+                current = 0;
+            }
+        }
+        if (max < current) max = current;
+        return max;
+    }
+}
