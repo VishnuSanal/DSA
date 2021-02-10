96ee7174959cebcb44bf5afb9d491d9656d3f7d4
Vishnu Sanal. T <t.v.s10123@gmail.com>
2021-02-10 13:14:18 +0530

    Updated SquaresOfSortedArray

--- src/SquaresOfSortedArray.java
+++ src/SquaresOfSortedArray.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 
 public class SquaresOfSortedArray {
 
@@ -17,22 +15,8 @@ public class SquaresOfSortedArray {
         for (int j = 0; j < ints.length; j++) {
             ints[j] = ints[j] * ints[j];
         }
-        return sortArray(ints);
+        Arrays.sort(ints);
+        return (ints);
     }
 
-    private static int[] sortArray(int[] ints) {
-
-        ArrayList<Integer> integerArrayList = new ArrayList<>();
-
-        for (int i : ints) {
-            integerArrayList.add(i);
-        }
-
-        Collections.sort(integerArrayList);
-
-        for (int i = 0; i < integerArrayList.size(); i++) {
-            ints[i] = integerArrayList.get(i);
-        }
-        return ints;
-    }
 }
