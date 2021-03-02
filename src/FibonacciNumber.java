e449fc68e9fd6e267736adff58cfb7612b7f847f
Vishnu Sanal. T <t.v.s10123@gmail.com>
2021-03-02 15:36:31 +0530

    FibonacciNumber

---
+++ src/FibonacciNumber.java
@@ -0,0 +1,16 @@
+public class FibonacciNumber {
+
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        System.out.println(fib(n));
+    }
+
+    public static int fib(int n) {
+        if (n <= 1)
+            return n;
+        else
+            return fib(n - 1) + fib(n - 2);
+    }
+}
